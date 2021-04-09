c4ac67fc06226597376326ba9f8f7e8badc96ce0
Kaspermn33 <32263696+Kaspermn33@users.noreply.github.com>
2021-04-09 17:53:44 +0200

    added sound commands

--- index.js
+++ index.js
@@ -6,11 +6,36 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
+client.on('message', async msg => {
+    if (!msg.content.startsWith(config.prefix) || msg.author.bot) return;
+    if (msg.content === '-ping') {
+        msg.reply('Pong!');
+    } else if(msg.content === '-ninja') {
+        playMp3("ninja", msg);
+    } else if(msg.content === '-illum' | msg.content === '-illuminati') {
+        playMp3("illuminati", msg)
+    }else if(msg.content === '-1hp') {
+        playMp3("1hp", msg)
+    }
 });
+
+async function playMp3(name, msg){
+    if (msg.member.voice.channel) {
+        const connection = await msg.member.voice.channel.join();
+        const dispatcher = connection.play('./sounds/' + name + '.mp3');
+
+        dispatcher.on('start', () => {
+            console.log(name + ' is now playing!');
+        });
+        
+        dispatcher.on('finish', () => {
+            console.log(name + ' has finished playing!');
+        });
+        
+        // Always remember to handle errors appropriately!
+        dispatcher.on('error', console.error);
+    }
+}
 
 
 client.login(config.token)
